a76f538a1b0ff93719392aa173d3d7760bb851ef
hakandl <1hakandagli@gmail.com>
2023-05-17 23:55:30 +0300

    Add new colors

--- lib/product/constant/colors.dart
+++ lib/product/constant/colors.dart
@@ -2,5 +2,9 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const white = Color(0xFFFFFFFF);
-  static const blue = Color(0xFF1D5EF4);
+  static const athensGray = Color(0xFFe2e1ec);
+  static const background = Color(0xFFE8ECF2);
+  static const blueRibbon = Color(0xFF1D5EF4);
+  static const persimmon = Color(0xFFFF5555);
+  static const caribbeanGreen = Color(0xFF02DAA7);
 }
