ad7351f7e9e0f1700b4695f3ad3db968cec32d28
hakandl <1hakandagli@gmail.com>
2023-05-17 23:54:14 +0300

    Update standart_textfield

--- lib/product/widgets/input/standart_textfield.dart
+++ lib/product/widgets/input/standart_textfield.dart
@@ -1,32 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:kartal/kartal.dart';
+import 'package:sneepy/product/constant/colors.dart';
 
-class StandartTextField extends StatefulWidget {
-  const StandartTextField({super.key, required this.text, this.obscureText});
+class StandartTextField extends StatelessWidget {
+  const StandartTextField({super.key, required this.text, this.obscureText, this.suffix});
   final String text;
   final bool? obscureText;
-
-  @override
-  State<StandartTextField> createState() => _StandartTextFieldState();
-}
-
-class _StandartTextFieldState extends State<StandartTextField> {
-  final focus = FocusNode();
+  final Widget? suffix;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 50,
       child: TextField(
-        focusNode: focus,
-        textAlignVertical: TextAlignVertical.bottom,
-        obscureText: widget.obscureText ?? false,
+        obscureText: obscureText ?? false,
         decoration: InputDecoration(
-          hintText: focus.hasFocus ? widget.text : '',
-          labelText: widget.text,
+          filled: true,
+          fillColor: AppColors.athensGray,
+          hintText: text,
+          suffixIcon: suffix,
           border: OutlineInputBorder(
             borderRadius: context.lowBorderRadius,
           ),
+          contentPadding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
         ),
       ),
     );
