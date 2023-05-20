ac7bf920b8d50466f25037a3c14e33864303f9f0
hakandl <1hakandagli@gmail.com>
2023-05-20 22:28:46 +0300

    update standart textfield and standart text button

--- lib/product/widgets/button/standart_text_button.dart
+++ lib/product/widgets/button/standart_text_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kartal/kartal.dart';
 import 'package:sneepy/product/constant/colors.dart';
+import 'package:sneepy/product/widgets/text/title_medium_text.dart';
 
 class StandartTextButton extends StatelessWidget {
   const StandartTextButton({super.key, required this.text, required this.onPressed, this.backgroundColor});
@@ -22,11 +23,9 @@ class StandartTextButton extends StatelessWidget {
         alignment: Alignment.center,
         width: double.infinity,
         height: 50,
-        child: Text(
-          text,
-          style: context.textTheme.titleMedium?.copyWith(
-            color: AppColors.white,
-          ),
+        child: TitleMediumText(
+          text: text,
+          color: AppColors.white,
         ),
       ),
     );
--- lib/product/widgets/input/standart_textfield.dart
+++ lib/product/widgets/input/standart_textfield.dart
@@ -3,9 +3,11 @@ import 'package:kartal/kartal.dart';
 import 'package:sneepy/product/constant/colors.dart';
 
 class StandartTextField extends StatelessWidget {
-  const StandartTextField({super.key, required this.text, this.obscureText, this.suffix});
+  const StandartTextField({super.key, required this.text, this.obscureText, this.suffix, this.prefix, this.enabled});
   final String text;
   final bool? obscureText;
+  final bool? enabled;
+  final Widget? prefix;
   final Widget? suffix;
 
   @override
@@ -16,8 +18,10 @@ class StandartTextField extends StatelessWidget {
         obscureText: obscureText ?? false,
         decoration: InputDecoration(
           filled: true,
+          enabled: enabled ?? true,
           fillColor: AppColors.athensGray,
           hintText: text,
+          prefixIcon: prefix,
           suffixIcon: suffix,
           border: OutlineInputBorder(
             borderRadius: context.lowBorderRadius,
