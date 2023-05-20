204ab18ed33795d55c55b47d96c0feff773614d9
hakandl <1hakandagli@gmail.com>
2023-05-20 22:27:38 +0300

    create standart container widget

---
+++ lib/product/widgets/container/standart_container.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:kartal/kartal.dart';
+
+import '../../constant/colors.dart';
+
+class StandartContainer extends StatelessWidget {
+  const StandartContainer({super.key, required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: context.paddingNormal,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: context.normalBorderRadius,
+      ),
+      child: child,
+    );
+  }
+}
