0b7b7e30ce33c356dd6f8294f3d1a3e88190421f
hakandl <1hakandagli@gmail.com>
2023-05-21 23:45:18 +0300

    create information view

---
+++ lib/feature/profie/settings/subview/information_view.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:kartal/kartal.dart';
+import 'package:sneepy/product/widgets/container/standart_container.dart';
+import 'package:sneepy/product/widgets/input/standart_textfield.dart';
+import 'package:sneepy/product/widgets/text/title_medium_text.dart';
+
+import '../../../../product/widgets/button/standart_text_button.dart';
+
+class InformationView extends StatelessWidget {
+  const InformationView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bazı Bilgiler'),
+      ),
+      body: Column(
+        children: [
+          context.emptySizedHeightBoxLow3x,
+          StandartContainer(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const TitleMediumText(text: 'İsim'),
+                context.emptySizedHeightBoxLow,
+                const StandartTextField(
+                  text: 'text',
+                  prefix: Icon(FontAwesomeIcons.user),
+                ),
+                context.emptySizedHeightBoxLow3x,
+                const TitleMediumText(text: 'Yaş'),
+                context.emptySizedHeightBoxLow,
+                const StandartTextField(
+                  text: 'text',
+                  prefix: Icon(FontAwesomeIcons.arrowUp19),
+                ),
+                context.emptySizedHeightBoxLow3x,
+                StandartTextButton(
+                  text: 'text',
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+          context.emptySizedHeightBoxLow3x,
+        ],
+      ),
+    );
+  }
+}
