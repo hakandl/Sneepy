db0c9be6558522df79aecd8e8a3af57500c683af
hakandl <1hakandagli@gmail.com>
2023-07-28 23:05:14 +0300

    created standart dialog widget

---
+++ lib/product/widgets/dialog/standart_dialog.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:kartal/kartal.dart';
+import 'package:sneepy/product/widgets/button/standart_text_button.dart';
+
+Future<T?> showStandartDialog<T>(
+  BuildContext context, {
+  String? title,
+  Widget? content,
+}) async {
+  return await showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: Text(title ?? ''),
+        content: content,
+        actions: [
+          StandartTextButton(
+            text: 'Anladım',
+            onPressed: () => context.pop(),
+          )
+        ],
+      );
+    },
+  );
+}
