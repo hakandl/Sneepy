5e5f59809861a29e1baa977b20c79a321bf82594
hakandl <1hakandagli@gmail.com>
2023-07-28 23:21:25 +0300

    AppColors updated

--- lib/product/constant/colors.dart
+++ lib/product/constant/colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const white = Color(0xFFFFFFFF);
+  static const ebonyClay = Color(0xFF222831);
   static const athensGray = Color(0xFFe2e1ec);
   static const background = Color(0xFFE8ECF2);
   static const blueRibbon = Color(0xFF1D5EF4);
