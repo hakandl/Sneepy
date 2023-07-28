77256e6d8d5387d098af3fe5e2909415e1ff81e1
hakandl <1hakandagli@gmail.com>
2023-07-28 23:16:58 +0300

    standart textfield updated

--- lib/product/widgets/input/standart_textfield.dart
+++ lib/product/widgets/input/standart_textfield.dart
@@ -4,7 +4,13 @@ import 'package:sneepy/product/constant/colors.dart';
 
 class StandartTextField extends StatelessWidget {
   StandartTextField(
-      {super.key, required this.text, this.obscureText, this.suffix, this.prefix, this.enabled, this.keyboardType});
+      {super.key,
+      required this.text,
+      this.obscureText,
+      this.suffix,
+      this.prefix,
+      this.enabled,
+      this.keyboardType});
   final TextEditingController controller = TextEditingController();
   final String text;
   final bool? obscureText;
@@ -15,25 +21,29 @@ class StandartTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 50,
-      child: TextField(
-        controller: controller,
-        obscureText: obscureText ?? false,
-        keyboardType: keyboardType,
-        textInputAction: TextInputAction.next,
-        decoration: InputDecoration(
-          filled: true,
-          enabled: enabled ?? true,
-          fillColor: AppColors.athensGray,
-          hintText: text,
-          prefixIcon: prefix,
-          suffixIcon: suffix,
-          border: OutlineInputBorder(
-            borderRadius: context.lowBorderRadius,
-          ),
-          contentPadding: const EdgeInsets.fromLTRB(12, 0, 12, 0),
+    return TextField(
+      controller: controller,
+      obscureText: obscureText ?? false,
+      keyboardType: keyboardType,
+      textInputAction: TextInputAction.next,
+      decoration: InputDecoration(
+        filled: true,
+        enabled: enabled ?? true,
+        fillColor: AppColors.athensGray,
+        hintText: text,
+        prefixIcon: prefix,
+        suffixIcon: suffix,
+        border: OutlineInputBorder(
+          borderRadius: context.lowBorderRadius,
         ),
+        disabledBorder: OutlineInputBorder(
+          borderRadius: context.lowBorderRadius,
+        ),
+        hintStyle: const TextStyle(
+          color: AppColors.ebonyClay,
+          fontWeight: FontWeight.w400,
+        ),
+        contentPadding: context.horizontalPaddingNormal,
       ),
     );
   }
