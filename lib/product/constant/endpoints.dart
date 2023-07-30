55c579a92a30063152cce82e6479ade50ec1afd6
hakandl <1hakandagli@gmail.com>
2023-07-30 23:33:31 +0300

    created endpoints

---
+++ lib/product/constant/endpoints.dart
@@ -0,0 +1,9 @@
+class ApiEndpoints {
+  static const me = 'me';
+  static const login = 'login';
+  static const register = 'register';
+  static const users = 'users';
+  static const sendFriendRequest = 'send-friend-request';
+  static const photos = 'photos';
+  static const countries = 'countries';
+}
