c88764732920ef9898e966bfb16d90de54b854f4
hakandl <1hakandagli@gmail.com>
2023-07-31 00:03:41 +0300

    models updated

--- lib/product/models/response_model.dart
+++ lib/product/models/response_model.dart
@@ -1,27 +1,26 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:sneepy/product/init/language/locale_keys.g.dart';
+
 class ResponseModel {
   bool? success;
-  // String? token;
   String? message;
   dynamic data;
 
   ResponseModel({
     this.success,
-    // this.token,
     this.message,
     this.data,
   });
 
   ResponseModel.fromJson(Map<String, dynamic> json) {
     success = json['success'] ?? false;
-    // token = json['token'];
-    message = json['message'] ?? 'bir hatalar varrr';
+    message = json['message'] ?? LocaleKeys.thereIsProblem.tr();
     data = json['data'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> json = <String, dynamic>{};
     json['success'] = success;
-    // json['token'] = token;
     json['message'] = message;
     json['data'] = data;
     return json;
