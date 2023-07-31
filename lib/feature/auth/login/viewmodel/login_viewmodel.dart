68e72cb9b455f4a6597c20bc968148c5877ce334
hakandl <1hakandagli@gmail.com>
2023-07-31 23:59:45 +0300

    login_viewmodel updated

--- lib/feature/auth/login/viewmodel/login_viewmodel.dart
+++ lib/feature/auth/login/viewmodel/login_viewmodel.dart
@@ -1,14 +1,40 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
+import 'package:sneepy/product/init/language/locale_keys.g.dart';
+import 'package:sneepy/product/models/response_model.dart';
+import 'package:sneepy/product/widgets/input/standart_textfield.dart';
+
+import '../../../../product/services/auth_service.dart';
 part 'login_viewmodel.g.dart';
 
 class LoginViewModel = _LoginViewModelBase with _$LoginViewModel;
 
 abstract class _LoginViewModelBase with Store {
+  final emailInput = StandartTextField(
+    text: LocaleKeys.auth_email.tr(),
+    keyboardType: TextInputType.emailAddress,
+  );
+  final passwordInput = StandartTextField(
+    text: LocaleKeys.auth_password.tr(),
+    obscureText: true,
+  );
+
   @observable
-  bool isShowPassword = true;
+  bool isLoading = false;
 
   @action
-  void showPassword() {
-    isShowPassword = !isShowPassword;
+  void changeLoading() {
+    isLoading = !isLoading;
+  }
+
+  Future<ResponseModel> login() async {
+    changeLoading();
+    final response = await AuthService().login(
+      email: emailInput.controller.text,
+      password: passwordInput.controller.text,
+    );
+    changeLoading();
+    return response;
   }
 }
--- lib/feature/auth/login/viewmodel/login_viewmodel.g.dart
+++ lib/feature/auth/login/viewmodel/login_viewmodel.g.dart
@@ -9,19 +9,19 @@ part of 'login_viewmodel.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$LoginViewModel on _LoginViewModelBase, Store {
-  late final _$isShowPasswordAtom =
-      Atom(name: '_LoginViewModelBase.isShowPassword', context: context);
+  late final _$isLoadingAtom =
+      Atom(name: '_LoginViewModelBase.isLoading', context: context);
 
   @override
-  bool get isShowPassword {
-    _$isShowPasswordAtom.reportRead();
-    return super.isShowPassword;
+  bool get isLoading {
+    _$isLoadingAtom.reportRead();
+    return super.isLoading;
   }
 
   @override
-  set isShowPassword(bool value) {
-    _$isShowPasswordAtom.reportWrite(value, super.isShowPassword, () {
-      super.isShowPassword = value;
+  set isLoading(bool value) {
+    _$isLoadingAtom.reportWrite(value, super.isLoading, () {
+      super.isLoading = value;
     });
   }
 
@@ -29,11 +29,11 @@ mixin _$LoginViewModel on _LoginViewModelBase, Store {
       ActionController(name: '_LoginViewModelBase', context: context);
 
   @override
-  void showPassword() {
+  void changeLoading() {
     final _$actionInfo = _$_LoginViewModelBaseActionController.startAction(
-        name: '_LoginViewModelBase.showPassword');
+        name: '_LoginViewModelBase.changeLoading');
     try {
-      return super.showPassword();
+      return super.changeLoading();
     } finally {
       _$_LoginViewModelBaseActionController.endAction(_$actionInfo);
     }
@@ -42,7 +42,7 @@ mixin _$LoginViewModel on _LoginViewModelBase, Store {
   @override
   String toString() {
     return '''
-isShowPassword: ${isShowPassword}
+isLoading: ${isLoading}
     ''';
   }
 }
