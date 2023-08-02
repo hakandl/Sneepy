853b3b1d4445233604eefd8ddf20676443bedc57
hakandl <1hakandagli@gmail.com>
2023-08-02 23:58:15 +0300

    countries_service created

---
+++ lib/product/services/countries_service.dart
@@ -0,0 +1,16 @@
+import 'package:sneepy/product/constant/endpoints.dart';
+import 'package:sneepy/product/models/countries_model.dart';
+import 'package:sneepy/product/services/service_manager.dart';
+
+class CountriesService {
+  Future<List<CountriesModel>> getAllCountries() async {
+    final response = await ServiceManager.makeRequest(
+      endpoint: ApiEndpoints.countries,
+      method: RequestMethod.GET,
+    );
+    final List jsonResponse = response.data;
+    final countries =
+        jsonResponse.map((e) => CountriesModel.fromJson(e)).toList();
+    return countries;
+  }
+}
