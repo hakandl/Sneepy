258c99a6de88ebad6ec9540536ebdef6a16a26b5
hakandl <1hakandagli@gmail.com>
2023-08-03 23:31:34 +0300

    auth_service updated

--- lib/product/services/auth_service.dart
+++ lib/product/services/auth_service.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:sneepy/product/constant/endpoints.dart';
 import 'package:sneepy/product/models/response_model.dart';
+import 'package:sneepy/product/models/user_model.dart';
 import 'package:sneepy/product/services/service_manager.dart';
 
 class AuthService {
@@ -22,12 +25,6 @@ class AuthService {
     required String name,
     required String email,
     required String password,
-    String? age,
-    String? gender,
-    String? country,
-    String? snapchat,
-    String? instagram,
-    String? twitter,
   }) async {
     final response = await ServiceManager.makeRequest(
       method: RequestMethod.POST,
@@ -36,13 +33,70 @@ class AuthService {
         'name': name,
         'email': email,
         'password': password,
-        'age': age,
-        'gender': gender,
-        'country': country,
-        'snapchat': snapchat,
-        'instagram': instagram,
-        'twitter': twitter,
       },
+    );
+    return response;
+  }
+
+  Future<UserModel> getMe() async {
+    final response = await ServiceManager.makeRequest(
+      method: RequestMethod.GET,
+      endpoint: ApiEndpoints.me,
+    );
+    final jsonResponse = UserModel.fromJson(response.data);
+    return jsonResponse;
+  }
+
+  Future<ResponseModel> updateMe({
+    String? name,
+    String? email,
+    String? password,
+    String? age,
+    String? gender,
+    String? country,
+    String? snapchat,
+    String? instagram,
+    String? twitter,
+  }) async {
+    final Map<String, dynamic> data = {
+      if (name != null) 'name': name,
+      if (email != null) 'email': email,
+      if (password != null) 'password': password,
+      'age': age,
+      'gender': gender,
+      'country': country,
+      'snapchat': snapchat,
+      'instagram': instagram,
+      'twitter': twitter,
+    };
+
+    final response = await ServiceManager.makeRequest(
+      method: RequestMethod.PUT,
+      endpoint: ApiEndpoints.me,
+      data: data,
+    );
+    return response;
+  }
+
+  Future<ResponseModel> addPhoto({
+    required File file,
+  }) async {
+    final response = await ServiceManager.makeRequest(
+      endpoint: ApiEndpoints.photos,
+      method: RequestMethod.POST,
+      file: File(file.path),
+    );
+    return response;
+  }
+
+  Future<ResponseModel> updatePhoto({
+    required File file,
+    String? photoId,
+  }) async {
+    final response = await ServiceManager.makeRequest(
+      endpoint: '${ApiEndpoints.photos}/$photoId',
+      method: RequestMethod.PUT,
+      file: File(file.path),
     );
     return response;
   }
