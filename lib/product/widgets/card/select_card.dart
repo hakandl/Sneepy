2a1d7f70bdc042d2fa19ae2635f93364ad10d217
hakandl <1hakandagli@gmail.com>
2023-08-04 23:17:53 +0300

    select_card updated

--- lib/product/widgets/card/select_card.dart
+++ lib/product/widgets/card/select_card.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
 class SelectCard extends StatelessWidget {
-  const SelectCard(
-      {super.key, required this.title, this.leading, this.trailing});
+  const SelectCard({
+    super.key,
+    required this.title,
+    this.leading,
+    this.trailing,
+    this.onTap,
+  });
   final Widget title;
   final Widget? leading;
   final Widget? trailing;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +19,7 @@ class SelectCard extends StatelessWidget {
       leading: leading,
       title: title,
       trailing: trailing,
+      onTap: onTap,
     );
   }
 }
