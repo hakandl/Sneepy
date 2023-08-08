b1e5bbbf1519b390bad3c00770b02e4a1672368e
hakandl <1hakandagli@gmail.com>
2023-08-09 01:10:37 +0300

    user_service updated

--- lib/product/services/user_service.dart
+++ lib/product/services/user_service.dart
@@ -1,4 +1,5 @@
 import 'package:sneepy/product/constants/endpoints.dart';
+import 'package:sneepy/product/models/response_model.dart';
 import 'package:sneepy/product/models/user_model.dart';
 import 'package:sneepy/product/services/service_manager.dart';
 
@@ -11,5 +12,27 @@ class UserService {
     final List jsonResponse = response.data;
     final users = jsonResponse.map((e) => UserModel.fromJson(e)).toList();
     return users;
+  }
+
+  Future<ResponseModel> sendFriendRequest({
+    required String userId,
+    required int sentType,
+  }) async {
+    final response = await ServiceManager.makeRequest(
+      method: RequestMethod.POST,
+      endpoint: '${ApiEndpoints.sendFriendRequest}/$userId/$sentType',
+    );
+    return response;
+  }
+
+  Future<ResponseModel> skipFriendRequest({
+    required String userId,
+  }) async {
+    final response = await ServiceManager.makeRequest(
+      method: RequestMethod.POST,
+      endpoint:
+          '${ApiEndpoints.users}/${ApiEndpoints.skipFriendRequest}/$userId',
+    );
+    return response;
   }
 }
