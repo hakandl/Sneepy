266b8ed6e67ba3618ab2daf3f6683caccd1fb797
hakandl <1hakandagli@gmail.com>
2023-08-10 01:50:15 +0300

    friends_view updated

--- lib/feature/friends/view/friends_view.dart
+++ lib/feature/friends/view/friends_view.dart
@@ -1,67 +1,131 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:kartal/kartal.dart';
 import 'package:sneepy/feature/friends/details/view/friend_details_view.dart';
-import 'package:sneepy/product/constant/colors.dart';
+import 'package:sneepy/feature/friends/viewmodel/friends_viewmodel.dart';
+import 'package:sneepy/product/constants/colors.dart';
+import 'package:sneepy/product/constants/enums/number.dart';
+import 'package:sneepy/product/constants/strings.dart';
+import 'package:sneepy/product/init/language/locale_keys.g.dart';
+import 'package:sneepy/product/models/user_model.dart';
 import 'package:sneepy/product/widgets/button/standart_text_button.dart';
+import 'package:sneepy/product/widgets/text/title_large_text.dart';
 import 'package:sneepy/product/widgets/text/title_medium_text.dart';
 
-class FriendsView extends StatelessWidget {
+class FriendsView extends StatefulWidget {
   const FriendsView({super.key});
+
+  @override
+  State<FriendsView> createState() => _FriendsViewState();
+}
+
+class _FriendsViewState extends State<FriendsView> {
+  final FriendsViewModel vm = FriendsViewModel();
+
+  @override
+  void initState() {
+    super.initState();
+    getUser();
+  }
+
+  Future<void> getUser() async {
+    await vm.getUser();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Arkadaşlık İstekleri'),
-      ),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            tileColor: index % 2 == 1 ? AppColors.background : AppColors.white,
-            contentPadding: EdgeInsets.symmetric(
-              vertical: context.verticalPaddingLow.vertical,
-              horizontal: context.horizontalPaddingLow.horizontal,
-            ),
-            leading: const CircleAvatar(
-              backgroundImage: NetworkImage(
-                  'https://media.gettyimages.com/id/1347431090/photo/fit-woman-standing-outdoors-after-a-late-afternoon-trail-run.jpg?s=612x612&w=gi&k=20&c=H9W6QxJoLP607i_BE7kT-VtxNGtaT7E6U4XIn03IHLg='),
-              radius: 48,
-            ),
-            title: Padding(
-              padding: context.onlyBottomPaddingLow,
-              child: const TitleMediumText(text: 'text'),
-            ),
-            subtitle: Row(
-              children: [
-                Expanded(
-                  child: SizedBox(
-                    height: 40,
-                    child: StandartTextButton(
-                      text: 'text',
-                      backgroundColor: AppColors.persimmon,
-                      onPressed: () {},
-                    ),
-                  ),
-                ),
-                context.emptySizedWidthBoxNormal,
-                Expanded(
-                  child: SizedBox(
-                    height: 40,
-                    child: StandartTextButton(
-                      text: 'text',
-                      backgroundColor: AppColors.caribbeanGreen,
-                      onPressed: () {},
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            onTap: () {
-              context.navigateToPage(const FriendDetailsView());
-            },
-          );
+      appBar: _appBar(),
+      body: Observer(
+        builder: (_) {
+          return vm.isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : RefreshIndicator(
+                  onRefresh: getUser,
+                  child: vm.user?.friendRequests.isNullOrEmpty == true
+                      ? Center(
+                          child: TitleLargeText(
+                            text: LocaleKeys
+                                .friendRequest_thereAreNoFriendRequests
+                                .tr(),
+                          ),
+                        )
+                      : ListView.builder(
+                          itemCount: vm.user?.friendRequests?.length ??
+                              NumberEnum.zero.value.toInt(),
+                          itemBuilder: (BuildContext context, int index) {
+                            final currentUser = vm.user?.friendRequests?[index];
+                            return ListTile(
+                              tileColor: index % NumberEnum.two.value ==
+                                      NumberEnum.one.value
+                                  ? AppColors.background
+                                  : AppColors.white,
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: context.verticalPaddingLow.vertical,
+                                horizontal:
+                                    context.horizontalPaddingLow.horizontal,
+                              ),
+                              leading: CircleAvatar(
+                                backgroundImage: CachedNetworkImageProvider(
+                                  currentUser?.photos?.firstOrNull?.photo ??
+                                      AppStrings.empty,
+                                ),
+                                radius: NumberEnum.fortyEight.value,
+                              ),
+                              title: Padding(
+                                padding: context.onlyBottomPaddingLow,
+                                child: TitleMediumText(
+                                  text: currentUser?.name ?? AppStrings.empty,
+                                ),
+                              ),
+                              subtitle: Row(
+                                children: [
+                                  Expanded(
+                                    child: SizedBox(
+                                      height: NumberEnum.forty.value,
+                                      child: StandartTextButton(
+                                        text: LocaleKeys.buttons_delete.tr(),
+                                        backgroundColor: AppColors.persimmon,
+                                        onPressed: () {},
+                                      ),
+                                    ),
+                                  ),
+                                  context.emptySizedWidthBoxNormal,
+                                  Expanded(
+                                    child: SizedBox(
+                                      height: NumberEnum.forty.value,
+                                      child: StandartTextButton(
+                                        text: LocaleKeys.buttons_accept.tr(),
+                                        backgroundColor:
+                                            AppColors.caribbeanGreen,
+                                        onPressed: () {},
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              onTap: () {
+                                context.navigateToPage(
+                                  FriendDetailsView(
+                                    user: vm.user ?? UserModel(),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        ),
+                );
         },
+      ),
+    );
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      title: Text(
+        LocaleKeys.friendRequest_friendRequests.tr(),
       ),
     );
   }
