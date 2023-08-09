5a3438c7b8eaac7ca3c21cae69ac0f85703f3f6d
hakandl <1hakandagli@gmail.com>
2023-08-10 01:36:56 +0300

    standart_dialog updated

--- lib/product/widgets/dialog/standart_dialog.dart
+++ lib/product/widgets/dialog/standart_dialog.dart
@@ -1,12 +1,17 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:kartal/kartal.dart';
 import 'package:sneepy/product/constants/strings.dart';
+import 'package:sneepy/product/init/language/locale_keys.g.dart';
 import 'package:sneepy/product/widgets/button/standart_text_button.dart';
 
 Future<T?> showStandartDialog<T>(
   BuildContext context, {
   String? title,
   Widget? content,
+  VoidCallback? onPressed,
+  String? buttonText,
+  Color? buttonColor,
 }) async {
   return await showDialog(
     context: context,
@@ -18,8 +23,9 @@ Future<T?> showStandartDialog<T>(
         content: content,
         actions: [
           StandartTextButton(
-            text: 'Anladım',
-            onPressed: () => context.pop(),
+            text: buttonText ?? LocaleKeys.buttons_gotIt.tr(),
+            backgroundColor: buttonColor,
+            onPressed: () => onPressed ?? context.pop(),
           )
         ],
       );
