ad0e5c6d1d35ba9d6a4a63d56c066d9a813430bd
hakandl <1hakandagli@gmail.com>
2023-08-10 02:01:27 +0300

    friend_card updated

--- lib/product/widgets/card/friend_card.dart
+++ lib/product/widgets/card/friend_card.dart
@@ -1,16 +1,21 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:kartal/kartal.dart';
-import 'package:sneepy/product/constant/colors.dart';
+import 'package:sneepy/product/constants/colors.dart';
+import 'package:sneepy/product/constants/enums/number.dart';
+import 'package:sneepy/product/constants/strings.dart';
+import 'package:sneepy/product/models/user_model.dart';
 import 'package:sneepy/product/widgets/text/headline_medium_text.dart';
 
 import '../../../feature/home/viewmodel/home_viewmodel.dart';
 import '../text/headline_small_text.dart';
 
 class FriendCard extends StatelessWidget {
-  FriendCard({super.key});
+  FriendCard({super.key, required this.user});
+  final UserModel user;
 
-  final PageController _swipeCardPageController = PageController(initialPage: 0);
+  final PageController _swipeCardPageController = PageController();
 
   final _vm = HomeViewModel();
 
@@ -26,40 +31,31 @@ class FriendCard extends StatelessWidget {
           children: [
             PageView.builder(
               controller: _swipeCardPageController,
-              itemCount: 3,
+              itemCount: user.photos?.length,
               physics: const NeverScrollableScrollPhysics(),
+              allowImplicitScrolling: true,
               itemBuilder: (context, index) {
-                List<String> images = [
-                  'https://media.gettyimages.com/id/1347431090/photo/fit-woman-standing-outdoors-after-a-late-afternoon-trail-run.jpg?s=612x612&w=gi&k=20&c=H9W6QxJoLP607i_BE7kT-VtxNGtaT7E6U4XIn03IHLg=',
-                  "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-                  "https://images.unsplash.com/photo-1506795660198-e95c77602129?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwd29tYW58ZW58MHx8MHx8fDA%3D&w=1000&q=80",
-                ];
+                final currentUser = user.photos?[index];
                 return SizedBox(
                   width: double.infinity,
                   height: double.infinity,
-                  child: InkWell(
-                    onTapUp: (details) {
-                      if (details.localPosition.direction > 1.0) {
-                        _swipeCardPageController.previousPage(
-                            duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
-                      } else if (details.localPosition.direction < 1.0) {
-                        _swipeCardPageController.nextPage(
-                            duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
-                      }
+                  child: GestureDetector(
+                    onHorizontalDragEnd: (DragEndDetails details) {
+                      onHorizontalDragChangePhoto(details, context);
                     },
-                    child: Image.network(
-                      images[index],
-                      fit: BoxFit.cover,
-                    ),
+                    onTapUp: (details) {
+                      onTapUpChangePhoto(details, context);
+                    },
+                    child: UserPhoto(currentUser: currentUser),
                   ),
                 );
               },
               onPageChanged: (index) {
-                _vm.progressValue = (index + 1) / 3;
+                pageChangedAndProgressValueUpdated(index);
               },
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              padding: context.paddingNormal,
               child: Observer(builder: (_) {
                 return LinearProgressIndicator(
                   value: _vm.progressValue,
@@ -69,39 +65,120 @@ class FriendCard extends StatelessWidget {
               }),
             ),
             Positioned(
-              bottom: 0,
+              bottom: NumberEnum.zero.value,
               child: IgnorePointer(
                 ignoring: false,
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 125,
-                  padding: context.paddingNormal,
-                  alignment: Alignment.bottomLeft,
-                  decoration: const BoxDecoration(
-                    color: AppColors.persimmon,
-                    gradient: LinearGradient(
-                      colors: [
-                        Colors.black,
-                        Colors.transparent,
-                      ],
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
-                    ),
-                  ),
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      HeadlineMediumText(text: 'text', color: AppColors.athensGray),
-                      HeadlineSmallText(text: 'text', color: AppColors.athensGray),
-                    ],
-                  ),
-                ),
+                child: UserInfoContainer(user: user),
               ),
             )
           ],
         ),
       ),
+    );
+  }
+
+  void pageChangedAndProgressValueUpdated(int index) {
+    _vm.progressValue = (index + NumberEnum.one.value) / NumberEnum.three.value;
+  }
+
+  void onHorizontalDragChangePhoto(
+      DragEndDetails details, BuildContext context) {
+    if (details.primaryVelocity! > NumberEnum.one.value) {
+      _swipeCardPageController.previousPage(
+        duration: context.durationLow,
+        curve: Curves.easeInOut,
+      );
+    } else if (details.primaryVelocity! < NumberEnum.one.value) {
+      _swipeCardPageController.nextPage(
+        duration: context.durationLow,
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+
+  void onTapUpChangePhoto(TapUpDetails details, BuildContext context) {
+    if (details.localPosition.direction > NumberEnum.one.value) {
+      _swipeCardPageController.previousPage(
+        duration: context.durationLow,
+        curve: Curves.easeInOut,
+      );
+    } else if (details.localPosition.direction < NumberEnum.one.value) {
+      _swipeCardPageController.nextPage(
+        duration: context.durationLow,
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+}
+
+class UserPhoto extends StatelessWidget {
+  const UserPhoto({
+    super.key,
+    required this.currentUser,
+  });
+
+  final Photos? currentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: currentUser?.photo ?? AppStrings.empty,
+      placeholder: (context, url) => const Center(
+        child: CircularProgressIndicator(),
+      ),
+      errorWidget: (context, url, error) => Icon(
+        Icons.error,
+        size: NumberEnum.sixty.value,
+      ),
+      fit: BoxFit.cover,
+    );
+  }
+}
+
+class UserInfoContainer extends StatelessWidget {
+  const UserInfoContainer({
+    super.key,
+    required this.user,
+  });
+
+  final UserModel user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: context.width,
+      height: NumberEnum.oneHundredTwentyFive.value,
+      padding: context.paddingNormal,
+      alignment: Alignment.bottomLeft,
+      decoration: const BoxDecoration(
+        color: AppColors.persimmon,
+        gradient: LinearGradient(
+          colors: [
+            Colors.black,
+            Colors.transparent,
+          ],
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+        ),
+      ),
+      child: userNameAndAgeColumn(),
+    );
+  }
+
+  Column userNameAndAgeColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        HeadlineMediumText(
+          text: user.name ?? AppStrings.empty,
+          color: AppColors.athensGray,
+        ),
+        HeadlineSmallText(
+          text: user.age.toString(),
+          color: AppColors.athensGray,
+        ),
+      ],
     );
   }
 }
