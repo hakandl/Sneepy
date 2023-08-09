521de6e44eae43deec651b1a9fca14ab7c93afa5
hakandl <1hakandagli@gmail.com>
2023-08-10 01:00:32 +0300

    hive_manager created

---
+++ lib/product/cache/hive_manager.dart
@@ -0,0 +1,26 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+enum BoxNames {
+  settings,
+}
+
+enum BoxKeyNames {
+  token,
+}
+
+class HiveManager {
+  static late Box _box;
+
+  static Future<void> init({required String boxName}) async {
+    await Hive.initFlutter();
+    _box = await Hive.openBox(boxName);
+  }
+
+  static Future<void> save({required String key, required String value}) async {
+    await _box.put(key, value);
+  }
+
+  static String? get({required String key}) {
+    return _box.get(key);
+  }
+}
