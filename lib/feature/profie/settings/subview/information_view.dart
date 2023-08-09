8520e79490519f4be176f9f5c8f4dc3927f6775b
hakandl <1hakandagli@gmail.com>
2023-08-10 01:37:39 +0300

    information_view updated

--- lib/feature/profie/settings/subview/information_view.dart
+++ lib/feature/profie/settings/subview/information_view.dart
@@ -1,51 +1,151 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:kartal/kartal.dart';
+import 'package:sneepy/feature/profie/settings/viewmodel/settings_viewmodel.dart';
+import 'package:sneepy/product/constants/enums/gender.dart';
+import 'package:sneepy/product/init/language/locale_keys.g.dart';
+import 'package:sneepy/product/models/user_model.dart';
+import 'package:sneepy/product/widgets/card/select_card.dart';
 import 'package:sneepy/product/widgets/container/standart_container.dart';
-import 'package:sneepy/product/widgets/input/standart_textfield.dart';
+import 'package:sneepy/product/widgets/modal/standart_modal_bottom_sheet.dart';
 import 'package:sneepy/product/widgets/text/title_medium_text.dart';
 
 import '../../../../product/widgets/button/standart_text_button.dart';
 
-class InformationView extends StatelessWidget {
+class InformationView extends StatefulWidget {
   const InformationView({super.key});
+
+  @override
+  State<InformationView> createState() => _InformationViewState();
+}
+
+class _InformationViewState extends State<InformationView> {
+  final SettingsViewModel vm = SettingsViewModel();
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  Future<void> getData() async {
+    await vm.getMe();
+    vm.initInformationView(vm.me ?? UserModel());
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Bazı Bilgiler'),
+      appBar: _appBar(),
+      body: Observer(
+        builder: (_) {
+          return vm.isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Column(
+                  children: [
+                    context.emptySizedHeightBoxLow3x,
+                    Flexible(
+                      child: PersonalInformationContainer(vm: vm),
+                    ),
+                    context.emptySizedHeightBoxLow3x,
+                  ],
+                );
+        },
       ),
-      body: Column(
+    );
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      title: Text(
+        LocaleKeys.profile_someInformation.tr(),
+      ),
+    );
+  }
+
+  Future<dynamic> genderModalBottomSheet(BuildContext context) {
+    return showStandartModalBottomSheet(
+      context,
+      isDynamicHeight: true,
+      child: Column(
         children: [
-          context.emptySizedHeightBoxLow3x,
-          StandartContainer(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const TitleMediumText(text: 'İsim'),
-                context.emptySizedHeightBoxLow,
-                StandartTextField(
-                  text: 'text',
-                  prefix: const Icon(FontAwesomeIcons.user),
-                ),
-                context.emptySizedHeightBoxLow3x,
-                const TitleMediumText(text: 'Yaş'),
-                context.emptySizedHeightBoxLow,
-                StandartTextField(
-                  text: 'text',
-                  prefix: const Icon(FontAwesomeIcons.arrowUp19),
-                ),
-                context.emptySizedHeightBoxLow3x,
-                StandartTextButton(
-                  text: 'text',
-                  onPressed: () {},
-                ),
-              ],
+          SelectCard(
+            leading: const Icon(Icons.female),
+            title: Text(
+              LocaleKeys.auth_register_woman.tr(),
             ),
+            trailing: Radio(
+              value: vm.gender == Gender.female.name,
+              groupValue: true,
+              onChanged: (value) => selectGender(context),
+            ),
+            onTap: () => selectGender(context),
           ),
-          context.emptySizedHeightBoxLow3x,
+          SelectCard(
+            leading: const Icon(Icons.male),
+            title: Text(
+              LocaleKeys.auth_register_man.tr(),
+            ),
+            trailing: Radio(
+              value: vm.gender == Gender.male.name,
+              groupValue: true,
+              onChanged: (value) => selectGender(context),
+            ),
+            onTap: () => selectGender(context),
+          ),
         ],
+      ),
+    );
+  }
+
+  void selectGender(BuildContext context) {
+    vm.selectGender();
+    context.pop();
+  }
+}
+
+class PersonalInformationContainer extends StatelessWidget {
+  const PersonalInformationContainer({
+    super.key,
+    required this.vm,
+  });
+
+  final SettingsViewModel vm;
+
+  @override
+  Widget build(BuildContext context) {
+    return StandartContainer(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TitleMediumText(
+              text: LocaleKeys.auth_register_name.tr(),
+            ),
+            context.emptySizedHeightBoxLow,
+            vm.nameInput,
+            context.emptySizedHeightBoxLow3x,
+            TitleMediumText(
+              text: LocaleKeys.auth_register_age.tr(),
+            ),
+            context.emptySizedHeightBoxLow,
+            vm.ageInput,
+            context.emptySizedHeightBoxLow3x,
+            TitleMediumText(
+              text: LocaleKeys.auth_register_gender.tr(),
+            ),
+            context.emptySizedHeightBoxLow,
+            vm.genderInput,
+            context.emptySizedHeightBoxLow3x,
+            StandartTextButton(
+              text: LocaleKeys.settings_updateInformation.tr(),
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
