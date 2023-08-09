4bff525d9f95c6722909125fa0798b6db3a46de5
hakandl <1hakandagli@gmail.com>
2023-08-10 01:51:58 +0300

    friend_details_view updated

--- lib/feature/friends/details/view/friend_details_view.dart
+++ lib/feature/friends/details/view/friend_details_view.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:kartal/kartal.dart';
+import 'package:sneepy/product/constants/strings.dart';
+import 'package:sneepy/product/models/user_model.dart';
 import 'package:sneepy/product/widgets/card/friend_card.dart';
 
 class FriendDetailsView extends StatelessWidget {
-  const FriendDetailsView({super.key});
+  const FriendDetailsView({super.key, required this.user});
+  final UserModel user;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('data'),
-      ),
+      appBar: _appBar(),
       body: SafeArea(
         child: Padding(
           padding: context.paddingLow,
-          child: FriendCard(),
+          child: FriendCard(
+            user: user,
+          ),
         ),
+      ),
+    );
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      title: Text(
+        user.name ?? AppStrings.empty,
       ),
     );
   }
