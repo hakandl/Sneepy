0a618072e54dab4216f4feb82daa2629fe78599a
hakandl <1hakandagli@gmail.com>
2023-08-19 23:52:44 +0300

    number enum created

---
+++ lib/product/constants/enums/number.dart
@@ -0,0 +1,24 @@
+enum NumberEnum {
+  zero(0),
+  zTwentyFive(0.25),
+  zTwo(0.2),
+  zThree(0.3),
+  one(1),
+  oneFive(1.50),
+  two(2),
+  three(3),
+  four(4),
+  ten(10),
+  eighteen(18),
+  thirty(30),
+  forty(40),
+  fortySeven(47),
+  fortyEight(48),
+  sixty(60),
+  sixtyFive(65),
+  oneHundredTwentyFive(125),
+  oneHundredTwentyEight(128);
+
+  const NumberEnum(this.value);
+  final double value;
+}
