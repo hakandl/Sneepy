943e93883a6664b5368eb3304ee50042540c54c2
hakandl <1hakandagli@gmail.com>
2023-08-20 01:39:46 +0300

    endpoints updated

--- lib/product/constants/endpoints.dart
+++ lib/product/constants/endpoints.dart
@@ -1,12 +1,13 @@
 class ApiEndpoints {
   static const me = 'me';
-  static const checkAuth = 'checkAuth';
+  static const checkAuth = 'check-auth';
   static const login = 'login';
   static const register = 'register';
   static const users = 'users';
   static const sendFriendRequest = 'send-friend-request';
   static const skipFriendRequest = 'skip-friend-request';
+  static const removeFriendRequest = 'remove-friend-request';
   static const photos = 'photos';
   static const countries = 'countries';
-  static const addPoint = 'addPoint';
+  static const addPoint = 'add-point';
 }
