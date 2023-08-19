8ae70a2e1fcd40ca989f8377e15ca43411999a42
hakandl <1hakandagli@gmail.com>
2023-08-19 23:49:59 +0300

    app strings created

---
+++ lib/product/constants/strings.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: constant_identifier_names
+
+class AppStrings {
+  static const empty = '';
+
+  // locale
+  static const tr = 'tr';
+  static const TR = 'TR';
+
+  static const en = 'en';
+  static const US = 'US';
+
+  static const ru = 'ru';
+  static const RU = 'RU';
+
+  static const de = 'de';
+  static const DE = 'DE';
+}
