7bd09c72d7d531ba85f4ce62503b74fae740472f
hakandl <1hakandagli@gmail.com>
2023-08-21 00:40:17 +0300

    added photo null check for friend_card

--- lib/product/widgets/card/friend_card.dart
+++ lib/product/widgets/card/friend_card.dart
@@ -1,12 +1,15 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:kartal/kartal.dart';
 import 'package:sneepy/product/constants/colors.dart';
 import 'package:sneepy/product/constants/enums/number.dart';
 import 'package:sneepy/product/constants/strings.dart';
+import 'package:sneepy/product/init/language/locale_keys.g.dart';
 import 'package:sneepy/product/models/user_model.dart';
 import 'package:sneepy/product/widgets/text/headline_medium_text.dart';
+import 'package:sneepy/product/widgets/text/title_large_text.dart';
 
 import '../../../feature/home/viewmodel/home_viewmodel.dart';
 import '../text/headline_small_text.dart';
@@ -21,64 +24,71 @@ class FriendCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: context.normalBorderRadius,
-      ),
-      child: ClipRRect(
-        borderRadius: context.normalBorderRadius,
-        child: Stack(
-          children: [
-            PageView.builder(
-              controller: _swipeCardPageController,
-              itemCount: user.photos?.length,
-              physics: const NeverScrollableScrollPhysics(),
-              allowImplicitScrolling: true,
-              itemBuilder: (context, index) {
-                final currentUser = user.photos?[index];
-                return SizedBox(
-                  width: double.infinity,
-                  height: double.infinity,
-                  child: GestureDetector(
-                    onHorizontalDragEnd: (DragEndDetails details) {
-                      onHorizontalDragChangePhoto(details, context);
+    return !user.photos.isNullOrEmpty
+        ? Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: context.normalBorderRadius,
+            ),
+            child: ClipRRect(
+              borderRadius: context.normalBorderRadius,
+              child: Stack(
+                children: [
+                  PageView.builder(
+                    controller: _swipeCardPageController,
+                    itemCount: user.photos?.length,
+                    physics: const NeverScrollableScrollPhysics(),
+                    allowImplicitScrolling: true,
+                    itemBuilder: (context, index) {
+                      final currentUser = user.photos?[index];
+                      return SizedBox(
+                        width: double.infinity,
+                        height: double.infinity,
+                        child: GestureDetector(
+                          onHorizontalDragEnd: (DragEndDetails details) {
+                            onHorizontalDragChangePhoto(details, context);
+                          },
+                          onTapUp: (details) {
+                            onTapUpChangePhoto(details, context);
+                          },
+                          child: UserPhoto(currentUser: currentUser),
+                        ),
+                      );
                     },
-                    onTapUp: (details) {
-                      onTapUpChangePhoto(details, context);
+                    onPageChanged: (index) {
+                      pageChangedAndProgressValueUpdated(index);
                     },
-                    child: UserPhoto(currentUser: currentUser),
                   ),
-                );
-              },
-              onPageChanged: (index) {
-                pageChangedAndProgressValueUpdated(index);
-              },
-            ),
-            Padding(
-              padding: context.paddingNormal,
-              child: Observer(builder: (_) {
-                return LinearProgressIndicator(
-                  value: _vm.progressValue,
-                  backgroundColor: Colors.black54,
-                  color: AppColors.athensGray,
-                );
-              }),
-            ),
-            Positioned(
-              bottom: NumberEnum.zero.value,
-              child: IgnorePointer(
-                ignoring: false,
-                child: UserInfoContainer(user: user),
+                  user.photos?.length == NumberEnum.one.value
+                      ? const SizedBox.shrink()
+                      : Padding(
+                          padding: context.paddingNormal,
+                          child: Observer(builder: (_) {
+                            return LinearProgressIndicator(
+                              value: _vm.progressValue,
+                              backgroundColor: Colors.black54,
+                              color: AppColors.athensGray,
+                            );
+                          }),
+                        ),
+                  Positioned(
+                    bottom: NumberEnum.zero.value,
+                    child: IgnorePointer(
+                      ignoring: false,
+                      child: UserInfoContainer(user: user),
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
-      ),
-    );
+            ),
+          )
+        : Center(child: TitleLargeText(text: LocaleKeys.thereIsProblem.tr()));
   }
 
   void pageChangedAndProgressValueUpdated(int index) {
-    _vm.progressValue = (index + NumberEnum.one.value) / NumberEnum.three.value;
+    _vm.progressValue = (index + NumberEnum.one.value) /
+        (user.photos?.length == NumberEnum.two.value
+            ? NumberEnum.two.value
+            : NumberEnum.three.value);
   }
 
   void onHorizontalDragChangePhoto(
