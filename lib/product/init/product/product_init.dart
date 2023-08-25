d739940bc2a067e5cb8b050a80f140a0c5280c92
hakandl <1hakandagli@gmail.com>
2023-08-26 00:58:38 +0300

    product_init updated

--- lib/product/init/product/product_init.dart
+++ lib/product/init/product/product_init.dart
@@ -1,6 +1,9 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:sneepy/product/cache/hive_manager.dart';
 import 'package:sneepy/product/init/language/localization_init.dart';
+import 'package:sneepy/product/init/product/firebase_settings.dart';
+import 'package:sneepy/product/init/product/revenuecat_init.dart';
 
 class ProductInit {
   final localizationInit = LocalizationInit();
@@ -8,5 +11,8 @@ class ProductInit {
   Future<void> init() async {
     WidgetsFlutterBinding.ensureInitialized();
     await EasyLocalization.ensureInitialized();
+    await RevenueCatInit().init();
+    await FirebaseSettings().init();
+    await HiveManager.init(boxName: BoxNames.settings.name);
   }
 }
