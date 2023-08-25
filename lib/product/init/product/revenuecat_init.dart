1ef8e12738449a6f477e1f629aa89b19221be080
hakandl <1hakandagli@gmail.com>
2023-08-26 00:55:20 +0300

    revenuecat init class created

---
+++ lib/product/init/product/revenuecat_init.dart
@@ -0,0 +1,15 @@
+import 'package:purchases_flutter/purchases_flutter.dart';
+import 'package:sneepy/product/constants/service.dart';
+
+class RevenueCatInit {
+  static const apiKey = ServiceConstants.revenueCatApiKey;
+
+  Future<void> init() async {
+    await Purchases.setLogLevel(LogLevel.info);
+
+    PurchasesConfiguration configuration;
+    configuration = PurchasesConfiguration(apiKey);
+
+    await Purchases.configure(configuration);
+  }
+}
