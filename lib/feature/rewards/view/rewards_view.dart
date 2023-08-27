ca8f94563def6cfa228e95fb222556ef8d527844
hakandl <1hakandagli@gmail.com>
2023-08-28 01:52:43 +0300

    rewards_view updated

--- lib/feature/rewards/view/rewards_view.dart
+++ lib/feature/rewards/view/rewards_view.dart
@@ -10,7 +10,6 @@ import 'package:sneepy/product/helpers/date.dart';
 import 'package:sneepy/product/init/language/locale_keys.g.dart';
 import 'package:sneepy/product/widgets/card/select_card.dart';
 import 'package:sneepy/product/widgets/container/standart_container.dart';
-import 'package:sneepy/product/widgets/dialog/standart_dialog.dart';
 import 'package:sneepy/product/widgets/text/title_medium_text.dart';
 
 class RewardsView extends StatefulWidget {
@@ -26,125 +25,163 @@ class _RewardsViewState extends State<RewardsView> {
   @override
   void initState() {
     super.initState();
-    getMe();
-    getOffers();
+    init();
   }
 
-  Future<void> getMe() async {
-    await vm.getMe();
-  }
-
-  Future<void> getOffers() async {
-    await vm.getOffers();
+  Future<void> init() async {
+    await vm.init();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(LocaleKeys.rewards_rewards.tr()),
-      ),
-      body: Column(
-        children: [
-          context.emptySizedHeightBoxLow3x,
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  StandartContainer(
-                    child: Column(
-                      children: [
-                        SelectCard(
-                          title: TitleMediumText(
-                            text: LocaleKeys.rewards_gift100Points.tr(),
-                          ),
-                          onTap: () async {
-                            final response = await vm.addPoint();
-                            if (response.success == true) {
-                              Fluttertoast.showToast(
-                                msg: LocaleKeys
-                                    .rewards_100GiftPointsAddedToYourAccount
-                                    .tr(),
-                              );
-                            } else if (response.success == false) {
-                              if (context.mounted) {
-                                showStandartDialog(
-                                  context,
-                                  title: LocaleKeys.thereIsProblem.tr(),
-                                  content: Text(
-                                    response.message ?? AppStrings.empty,
-                                  ),
-                                );
-                              }
-                            }
-                          },
+      appBar: _appBar(),
+      body: Observer(
+        builder: (_) {
+          return vm.loading.isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Column(
+                  children: [
+                    context.emptySizedHeightBoxLow3x,
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            RewardsGiftContainer(vm: vm),
+                            context.emptySizedHeightBoxNormal,
+                            RewardedAdContainer(vm: vm),
+                            context.emptySizedHeightBoxNormal,
+                            PackagesContainer(vm: vm),
+                          ],
                         ),
-                        Observer(
-                          builder: (_) {
-                            return vm.isLoading
-                                ? const CircularProgressIndicator()
-                                : vm.me?.isFreePoint == false
-                                    ? Column(
-                                        children: [
-                                          context.emptySizedHeightBoxLow,
-                                          Padding(
-                                            padding:
-                                                context.horizontalPaddingLow,
-                                            child: TitleMediumText(
-                                                text:
-                                                    '${LocaleKeys.rewards_youCannotReceiveNewGiftPointsBeforeThisDate.tr()} ${vm.me?.pointTimeToScore.convertDateString()}'),
-                                          ),
-                                        ],
-                                      )
-                                    : const SizedBox.shrink();
-                          },
-                        )
-                      ],
+                      ),
                     ),
-                  ),
-                  context.emptySizedHeightBoxNormal,
-                  StandartContainer(
-                    child: Observer(builder: (_) {
-                      return vm.isLoading
-                          ? const Center(child: CircularProgressIndicator())
-                          : ListView.builder(
-                              itemCount: vm.packages?.length ??
-                                  NumberEnum.zero.value.toInt(),
-                              shrinkWrap: true,
-                              physics: const NeverScrollableScrollPhysics(),
-                              itemBuilder: (BuildContext context, int index) {
-                                final currentProduct =
-                                    vm.packages?[index].storeProduct;
-                                return SelectCard(
-                                  leading: TitleMediumText(
-                                    text: currentProduct?.priceString ??
-                                        AppStrings.empty,
-                                  ),
-                                  title: TitleMediumText(
-                                    text: currentProduct?.title ??
-                                        AppStrings.empty,
-                                  ),
-                                  subtitle: TitleMediumText(
-                                    text: currentProduct?.description ??
-                                        AppStrings.empty,
-                                  ),
-                                  onTap: () async {
-                                    await vm.prodPurchase(
-                                      vm.packages![index],
-                                      context,
-                                    );
-                                  },
-                                );
-                              },
-                            );
-                    }),
-                  ),
-                ],
-              ),
+                    context.emptySizedHeightBoxLow3x,
+                  ],
+                );
+        },
+      ),
+    );
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      title: Text(
+        LocaleKeys.rewards_rewards.tr(),
+      ),
+    );
+  }
+}
+
+class RewardsGiftContainer extends StatelessWidget {
+  const RewardsGiftContainer({
+    super.key,
+    required this.vm,
+  });
+
+  final RewardsViewModel vm;
+
+  @override
+  Widget build(BuildContext context) {
+    return StandartContainer(
+      child: Column(
+        children: [
+          SelectCard(
+            title: TitleMediumText(
+              text: LocaleKeys.rewards_gift100Points.tr(),
             ),
+            onTap: () async {
+              final response = await vm.addPoint();
+              if (response.success == true) {
+                Fluttertoast.showToast(
+                  msg: LocaleKeys.rewards_100GiftPointsAddedToYourAccount.tr(),
+                );
+              } else if (response.success == false) {
+                Fluttertoast.showToast(
+                  msg: response.message ?? AppStrings.empty,
+                );
+              }
+            },
           ),
-          context.emptySizedHeightBoxLow3x,
+          vm.me?.isFreePoint == false
+              ? Column(
+                  children: [
+                    context.emptySizedHeightBoxLow,
+                    Padding(
+                      padding: context.horizontalPaddingLow,
+                      child: TitleMediumText(
+                        text:
+                            '${LocaleKeys.rewards_youCannotReceiveNewGiftPointsBeforeThisDate.tr()} ${vm.me?.pointTimeToScore.convertDateString()}',
+                      ),
+                    ),
+                  ],
+                )
+              : const SizedBox.shrink()
         ],
+      ),
+    );
+  }
+}
+
+class RewardedAdContainer extends StatelessWidget {
+  const RewardedAdContainer({
+    super.key,
+    required this.vm,
+  });
+
+  final RewardsViewModel vm;
+
+  @override
+  Widget build(BuildContext context) {
+    return StandartContainer(
+      child: SelectCard(
+        title: TitleMediumText(
+          text: LocaleKeys.rewards_watchAnAdAndGet100PointsForFree.tr(),
+        ),
+        onTap: () {
+          vm.showRewardedAd();
+        },
+      ),
+    );
+  }
+}
+
+class PackagesContainer extends StatelessWidget {
+  const PackagesContainer({
+    super.key,
+    required this.vm,
+  });
+
+  final RewardsViewModel vm;
+
+  @override
+  Widget build(BuildContext context) {
+    return StandartContainer(
+      child: ListView.builder(
+        itemCount: vm.packages?.length ?? NumberEnum.zero.value.toInt(),
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (BuildContext context, int index) {
+          final currentProduct = vm.packages?[index].storeProduct;
+          return SelectCard(
+            leading: TitleMediumText(
+              text: currentProduct?.priceString ?? AppStrings.empty,
+            ),
+            title: TitleMediumText(
+              text: currentProduct?.title ?? AppStrings.empty,
+            ),
+            subtitle: TitleMediumText(
+              text: currentProduct?.description ?? AppStrings.empty,
+            ),
+            onTap: () async {
+              await vm.productPurchase(
+                vm.packages![index],
+              );
+            },
+          );
+        },
       ),
     );
   }
