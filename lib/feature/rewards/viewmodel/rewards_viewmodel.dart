a9910bee24b595e06c632505d057f2fff89a66fe
hakandl <1hakandagli@gmail.com>
2023-08-28 01:47:16 +0300

    rewarded ad function created

--- lib/feature/rewards/viewmodel/rewards_viewmodel.dart
+++ lib/feature/rewards/viewmodel/rewards_viewmodel.dart
@@ -1,20 +1,22 @@
 import 'dart:async';
 
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:mobx/mobx.dart';
 import 'package:purchases_flutter/errors.dart';
 import 'package:purchases_flutter/models/package_wrapper.dart';
 import 'package:purchases_flutter/purchases_flutter.dart' as purchase;
 import 'package:sneepy/product/constants/enums/number.dart';
 import 'package:sneepy/product/constants/enums/point.dart';
+import 'package:sneepy/product/constants/service.dart';
 import 'package:sneepy/product/init/language/locale_keys.g.dart';
 import 'package:sneepy/product/models/response_model.dart';
 import 'package:sneepy/product/models/user_model.dart';
 import 'package:sneepy/product/services/auth_service.dart';
 import 'package:sneepy/product/services/user_service.dart';
-import 'package:sneepy/product/widgets/dialog/standart_dialog.dart';
+import 'package:sneepy/product/utils/loading.dart';
 
 import '../../../product/services/revenuecat_service.dart';
 part 'rewards_viewmodel.g.dart';
@@ -22,42 +24,79 @@ part 'rewards_viewmodel.g.dart';
 class RewardsViewModel = _RewardsViewModelBase with _$RewardsViewModel;
 
 abstract class _RewardsViewModelBase with Store {
-  @observable
-  bool isLoading = false;
-
-  @action
-  void changeLoading() {
-    isLoading = !isLoading;
-  }
-
+  final LoadingUtil loading = LoadingUtil();
+  RewardedAd? rewardedAd;
   List<Package>? packages;
-
   UserModel? me;
 
+  Future<void> init() async {
+    await getMe();
+    await getOffers();
+    await initGoogleMobileAds();
+    initRewardedAd();
+  }
+
+  Future<InitializationStatus> initGoogleMobileAds() {
+    return MobileAds.instance.initialize();
+  }
+
   Future<void> getMe() async {
-    changeLoading();
+    loading.changeLoading();
     me = await AuthService().getMe();
-    changeLoading();
+    loading.changeLoading();
   }
 
   Future<ResponseModel> addPoint() async {
-    changeLoading();
+    loading.changeLoading();
     final response = await UserService().addPoint();
-    changeLoading();
+    loading.changeLoading();
     return response;
   }
 
+  void initRewardedAd() {
+    RewardedAd.load(
+        adUnitId: ServiceConstants.adUnitId,
+        request: const AdRequest(),
+        rewardedAdLoadCallback: RewardedAdLoadCallback(
+          onAdLoaded: (ad) {
+            ad.fullScreenContentCallback = FullScreenContentCallback(
+              onAdDismissedFullScreenContent: (ad) async {
+                ad.dispose();
+                rewardedAd = null;
+                initRewardedAd();
+                await updatePoint(point: NumberEnum.oneHundred.value.toInt());
+                Fluttertoast.showToast(
+                  msg: LocaleKeys.rewards_100GiftPointsAddedToYourAccount.tr(),
+                );
+              },
+            );
+            rewardedAd = ad;
+          },
+          onAdFailedToLoad: (LoadAdError error) {
+            Fluttertoast.showToast(
+              msg: LocaleKeys.rewards_thereWasAnErrorLoadingTheAd.tr(),
+            );
+          },
+        ));
+  }
+
+  void showRewardedAd() {
+    rewardedAd?.show(
+      onUserEarnedReward: (ad, reward) {},
+    );
+  }
+
   Future<void> getOffers() async {
-    changeLoading();
+    loading.changeLoading();
     final offerings = await RevenueCatService.getOffers();
     packages = offerings
         .map((offer) => offer.availablePackages)
         .expand((pair) => pair)
         .toList();
-    changeLoading();
+    loading.changeLoading();
   }
 
-  Future<void> prodPurchase(Package package, BuildContext context) async {
+  Future<void> productPurchase(Package package) async {
     try {
       purchase.CustomerInfo purchaserInfo =
           await purchase.Purchases.purchasePackage(package);
@@ -87,31 +126,24 @@ abstract class _RewardsViewModelBase with Store {
           point = NumberEnum.tenThousand.value.toInt();
         }
         await updatePoint(point: point ?? NumberEnum.zero.value.toInt());
-        if (context.mounted) {
-          showStandartDialog(
-            context,
-            title: LocaleKeys.successful.tr(),
-            content: Text(
+        Fluttertoast.showToast(
+          msg:
               LocaleKeys.rewards_paymentSuccessfulPointsAddedToYourAccount.tr(),
-            ),
-          );
-        }
+        );
       }
     } on PlatformException catch (e) {
       var errorCode = PurchasesErrorHelper.getErrorCode(e);
       if (errorCode != PurchasesErrorCode.purchaseCancelledError) {
-        showStandartDialog(
-          context,
-          title: LocaleKeys.thereIsProblem.tr(),
-          content: Text(
-            LocaleKeys.rewards_paymentFailed.tr(),
-          ),
+        Fluttertoast.showToast(
+          msg: LocaleKeys.rewards_paymentFailed.tr(),
         );
       }
     }
   }
 
   Future<void> updatePoint({required int point}) async {
+    loading.changeLoading();
     await UserService().updatePoint(point: point);
+    loading.changeLoading();
   }
 }
