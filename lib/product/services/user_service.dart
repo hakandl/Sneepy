c66e31ec1ab64653b35febbbdb48b7a0229bb590
hakandl <1hakandagli@gmail.com>
2023-08-28 01:55:19 +0300

    added params fields for getUsers (age, gender, country)

--- lib/product/services/user_service.dart
+++ lib/product/services/user_service.dart
@@ -1,3 +1,4 @@
+import 'package:sneepy/product/cache/hive_manager.dart';
 import 'package:sneepy/product/constants/endpoints.dart';
 import 'package:sneepy/product/models/response_model.dart';
 import 'package:sneepy/product/models/user_model.dart';
@@ -6,9 +7,13 @@ import 'package:sneepy/product/services/service_manager.dart';
 class UserService {
   Future<List<UserModel>> getUsers() async {
     final response = await ServiceManager.makeRequest(
-      method: RequestMethod.GET,
-      endpoint: ApiEndpoints.users,
-    );
+        method: RequestMethod.GET,
+        endpoint: ApiEndpoints.users,
+        params: {
+          'age': HiveManager.get(key: BoxKeyNames.age.name),
+          'gender': HiveManager.get(key: BoxKeyNames.gender.name),
+          'country': HiveManager.get(key: BoxKeyNames.country.name),
+        });
     final List jsonResponse = response.data;
     final users = jsonResponse.map((e) => UserModel.fromJson(e)).toList();
     return users;
