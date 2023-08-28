250f430cccccbec4fbbc5d301ca28ff8f47e82c0
hakandl <1hakandagli@gmail.com>
2023-08-29 01:55:22 +0300

    theme created

---
+++ lib/product/init/theme/theme.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:sneepy/product/constants/enums/number.dart';
+import 'package:sneepy/product/init/theme/theme_colors.dart';
+
+class SneepyTheme {
+  late ThemeData theme;
+  final SneepyThemeColors sneepyThemeColors = SneepyThemeColors();
+
+  SneepyTheme(BuildContext context) {
+    theme = ThemeData(
+      useMaterial3: true,
+      colorScheme: _colorScheme,
+      scaffoldBackgroundColor: sneepyThemeColors.background,
+      appBarTheme: _appBar(context),
+    );
+  }
+
+  ColorScheme get _colorScheme => ColorScheme(
+        brightness: sneepyThemeColors.brightnessLight,
+        primary: sneepyThemeColors.blueRibbon,
+        onPrimary: sneepyThemeColors.white,
+        secondary: sneepyThemeColors.caribbeanGreen,
+        onSecondary: sneepyThemeColors.caribbeanGreen,
+        error: sneepyThemeColors.persimmon,
+        onError: sneepyThemeColors.persimmon,
+        background: sneepyThemeColors.athensGray,
+        onBackground: sneepyThemeColors.background,
+        surface: sneepyThemeColors.ebonyClay,
+        onSurface: sneepyThemeColors.ebonyClay,
+      );
+
+  AppBarTheme _appBar(BuildContext context) {
+    return AppBarTheme(
+        centerTitle: true,
+        backgroundColor: sneepyThemeColors.blueRibbon,
+        scrolledUnderElevation: NumberEnum.ten.value,
+        iconTheme: IconThemeData(
+          color: sneepyThemeColors.white,
+        ),
+        actionsIconTheme: IconThemeData(
+          color: sneepyThemeColors.white,
+        ),
+        titleTextStyle: Theme.of(context).textTheme.titleLarge?.copyWith(
+              color: sneepyThemeColors.white,
+            ));
+  }
+}
---
+++ lib/product/init/theme/theme_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SneepyThemeColors {
+  /* static const white = Color(0xFFFFFFFF);
+  static const ebonyClay = Color(0xFF222831);
+  static const athensGray = Color(0xFFe2e1ec);
+  static const background = Color(0xFFE8ECF2);
+  static const blueRibbon = Color(0xFF1D5EF4);
+  static const persimmon = Color(0xFFFF5555);
+  static const caribbeanGreen = Color(0xFF02DAA7); */
+
+  // final Color aaaa = Color(0xFFFF5555);
+  final Color white = const Color(0xFFFFFFFF);
+  final Color ebonyClay = const Color(0xFF222831);
+  final Color athensGray = const Color(0xFFe2e1ec);
+  final Color background = const Color(0xFFE8ECF2);
+  final Color blueRibbon = const Color(0xFF1D5EF4);
+  final Color persimmon = const Color(0xFFFF5555);
+  final Color caribbeanGreen = const Color(0xFF02DAA7);
+
+  final Brightness brightnessLight = Brightness.light;
+  final SystemUiOverlayStyle systemOverlayStyle = SystemUiOverlayStyle.light;
+
+  final Color transparent = Colors.transparent;
+}
