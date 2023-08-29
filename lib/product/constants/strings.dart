a0b25cdc9b8f7f180119a486c4069c8355b31a53
hakandl <1hakandagli@gmail.com>
2023-08-30 01:01:03 +0300

    app strings updated

--- lib/product/constants/strings.dart
+++ lib/product/constants/strings.dart
@@ -1,27 +1,28 @@
 // ignore_for_file: constant_identifier_names
 
 class AppStrings {
-  static const empty = '';
+  static const String empty = '';
 
   // locales
-  static const tr = 'tr';
-  static const TR = 'TR';
+  static const String tr = 'tr';
+  static const String TR = 'TR';
 
-  static const en = 'en';
-  static const US = 'US';
+  static const String en = 'en';
+  static const String US = 'US';
 
-  static const ru = 'ru';
-  static const RU = 'RU';
+  static const String ru = 'ru';
+  static const String RU = 'RU';
 
-  static const de = 'de';
-  static const DE = 'DE';
+  static const String de = 'de';
+  static const String DE = 'DE';
 
   // socials
-  static const snapchat = 'https://www.snapchat.com/add/';
-  static const instagram = 'https://www.instagram.com/';
-  static const twitter = 'https://www.twitter.com/';
+  static const String snapchat = 'https://www.snapchat.com/add/';
+  static const String instagram = 'https://www.instagram.com/';
+  static const String twitter = 'https://www.twitter.com/';
 
   // assets
-  static const userNotPhoto =
+  static const String sneepyLogo = 'assets/images/sneepy-logo.png';
+  static const String userNotPhoto =
       'https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=';
 }
