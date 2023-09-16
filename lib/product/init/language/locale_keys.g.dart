6d7a6be4a7c74522916d79aea8000f8e065775aa
hakandl <1hakandagli@gmail.com>
2023-09-17 00:47:01 +0300

    languages updated and new languages added

--- lib/product/init/language/locale_keys.g.dart
+++ lib/product/init/language/locale_keys.g.dart
@@ -1,8 +1,6 @@
 // DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
 
-// ignore_for_file: constant_identifier_names
-
-abstract class LocaleKeys {
+abstract class  LocaleKeys {
   static const buttons_login = 'buttons.login';
   static const buttons_logout = 'buttons.logout';
   static const buttons_register = 'buttons.register';
@@ -16,17 +14,15 @@ abstract class LocaleKeys {
   static const auth_email = 'auth.email';
   static const auth_password = 'auth.password';
   static const auth_login_login = 'auth.login.login';
-  static const auth_login_dontYouHaveAnAccount =
-      'auth.login.dontYouHaveAnAccount';
+  static const auth_login_dontYouHaveAnAccount = 'auth.login.dontYouHaveAnAccount';
   static const auth_login = 'auth.login';
-  static const auth_register_youMustEnterThisRequiredInformationToRegister =
-      'auth.register.youMustEnterThisRequiredInformationToRegister';
-  static const auth_register_nowLetsContinueWithSomeOfYourPersonalInformation =
-      'auth.register.nowLetsContinueWithSomeOfYourPersonalInformation';
-  static const auth_register_enterTheUsernameOfTheSocialMediaAccountsYouWantToShare =
-      'auth.register.enterTheUsernameOfTheSocialMediaAccountsYouWantToShare';
-  static const auth_register_finallyLetsAddPhoto =
-      'auth.register.finallyLetsAddPhoto';
+  static const auth_register_nameMustBeMinimumOf2Characters = 'auth.register.nameMustBeMinimumOf2Characters';
+  static const auth_register_pleaseEnterValidEmail = 'auth.register.pleaseEnterValidEmail';
+  static const auth_register_passwordMustBeMinimumOf6Characters = 'auth.register.passwordMustBeMinimumOf6Characters';
+  static const auth_register_youMustEnterThisRequiredInformationToRegister = 'auth.register.youMustEnterThisRequiredInformationToRegister';
+  static const auth_register_nowLetsContinueWithSomeOfYourPersonalInformation = 'auth.register.nowLetsContinueWithSomeOfYourPersonalInformation';
+  static const auth_register_enterTheUsernameOfTheSocialMediaAccountsYouWantToShare = 'auth.register.enterTheUsernameOfTheSocialMediaAccountsYouWantToShare';
+  static const auth_register_finallyLetsAddPhoto = 'auth.register.finallyLetsAddPhoto';
   static const auth_register_name = 'auth.register.name';
   static const auth_register_age = 'auth.register.age';
   static const auth_register_gender = 'auth.register.gender';
@@ -36,25 +32,27 @@ abstract class LocaleKeys {
   static const auth_register_snapchat = 'auth.register.snapchat';
   static const auth_register_instagram = 'auth.register.instagram';
   static const auth_register_twitter = 'auth.register.twitter';
-  static const auth_register_toRegisterYouNeedToUploadPhotoToYourProfile =
-      'auth.register.toRegisterYouNeedToUploadPhotoToYourProfile';
+  static const auth_register_toRegisterYouNeedToUploadPhotoToYourProfile = 'auth.register.toRegisterYouNeedToUploadPhotoToYourProfile';
   static const auth_register = 'auth.register';
   static const auth = 'auth';
   static const home_noUsersFound = 'home.noUsersFound';
+  static const home_usersPhotoCouldNotBeUploaded = 'home.usersPhotoCouldNotBeUploaded';
+  static const home_youCanReachYourProfileHere = 'home.youCanReachYourProfileHere';
+  static const home_clickHereNowToEarnMorePoints = 'home.clickHereNowToEarnMorePoints';
+  static const home_youCanSeeYourFriendRequestsHere = 'home.youCanSeeYourFriendRequestsHere';
+  static const home_youCanSeeOtherPhotosIfAvailableBySwipingRightAndLeftOrClickingOnThem = 'home.youCanSeeOtherPhotosIfAvailableBySwipingRightAndLeftOrClickingOnThem';
+  static const home_youCanUseThisButtonToPassUser = 'home.youCanUseThisButtonToPassUser';
+  static const home_youCanUseThisButtonToSendRequestToUser = 'home.youCanUseThisButtonToSendRequestToUser';
   static const home = 'home';
   static const profile_profile = 'profile.profile';
   static const profile_someInformation = 'profile.someInformation';
   static const profile_socialAccounts = 'profile.socialAccounts';
   static const profile = 'profile';
   static const friendRequest_friendRequests = 'friendRequest.friendRequests';
-  static const friendRequest_thereAreNoFriendRequests =
-      'friendRequest.thereAreNoFriendRequests';
-  static const friendRequest_usersSnapchatAccountNotFound =
-      'friendRequest.usersSnapchatAccountNotFound';
-  static const friendRequest_usersInstagramAccountNotFound =
-      'friendRequest.usersInstagramAccountNotFound';
-  static const friendRequest_usersTwitterAccountNotFound =
-      'friendRequest.usersTwitterAccountNotFound';
+  static const friendRequest_thereAreNoFriendRequests = 'friendRequest.thereAreNoFriendRequests';
+  static const friendRequest_usersSnapchatAccountNotFound = 'friendRequest.usersSnapchatAccountNotFound';
+  static const friendRequest_usersInstagramAccountNotFound = 'friendRequest.usersInstagramAccountNotFound';
+  static const friendRequest_usersTwitterAccountNotFound = 'friendRequest.usersTwitterAccountNotFound';
   static const friendRequest = 'friendRequest';
   static const settings_settings = 'settings.settings';
   static const settings_personalInformation = 'settings.personalInformation';
@@ -67,13 +65,27 @@ abstract class LocaleKeys {
   static const settings_updateUsernames = 'settings.updateUsernames';
   static const settings_updatePhotos = 'settings.updatePhotos';
   static const settings_deleteAccount = 'settings.deleteAccount';
-  static const settings_areYouSureYouWantToDeleteYourAccount =
-      'settings.areYouSureYouWantToDeleteYourAccount';
+  static const settings_areYouSureYouWantToDeleteYourAccount = 'settings.areYouSureYouWantToDeleteYourAccount';
+  static const settings_privacyPolicy = 'settings.privacyPolicy';
   static const settings = 'settings';
   static const languages_turkish = 'languages.turkish';
   static const languages_english = 'languages.english';
   static const languages_russian = 'languages.russian';
   static const languages_german = 'languages.german';
   static const languages = 'languages';
+  static const rewards_rewards = 'rewards.rewards';
+  static const rewards_gift100Points = 'rewards.gift100Points';
+  static const rewards_paidPoints = 'rewards.paidPoints';
+  static const rewards_youCannotReceiveNewGiftPointsBeforeThisDate = 'rewards.youCannotReceiveNewGiftPointsBeforeThisDate';
+  static const rewards_100GiftPointsAddedToYourAccount = 'rewards.100GiftPointsAddedToYourAccount';
+  static const rewards_paymentSuccessfulPointsAddedToYourAccount = 'rewards.paymentSuccessfulPointsAddedToYourAccount';
+  static const rewards_paymentFailed = 'rewards.paymentFailed';
+  static const rewards_watchAnAdAndGet100PointsForFree = 'rewards.watchAnAdAndGet100PointsForFree';
+  static const rewards_thereWasAnErrorLoadingTheAd = 'rewards.thereWasAnErrorLoadingTheAd';
+  static const rewards = 'rewards';
+  static const successful = 'successful';
   static const thereIsProblem = 'thereIsProblem';
+  static const noInternetConnection = 'noInternetConnection';
+  static const pleaseCheckYourInternetConnection = 'pleaseCheckYourInternetConnection';
+
 }
